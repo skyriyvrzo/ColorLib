cf75fbef9a6fc9da0104d471e3ecc7f374caddf3
GE77 <skyriyvrzo@gmail.com>
2023-09-29 17:45:59 +0700

    152654515235

---
+++ src/com/get/colorlib/ColorLib.java
@@ -0,0 +1,31 @@
+package com.get.colorlib;
+
+import java.awt.*;
+
+public class ColorLib {
+
+        public static final Color Snow = new Color(255, 250, 250);
+        public static final Color GhostWhite = new Color(248,248,255);
+        public static final Color WhiteSmoke = new Color(245,245,245);
+        public static final Color Gainsboro = new Color(220,220,220);
+        public static final Color FloralWhite = new Color(255,250,240);
+        public static final Color OldLace = new Color(253,245,230);
+        public static final Color Linen = new Color(250,240,230);
+        public static final Color AntiqueWhite = new Color(250,235,215);
+        public static final Color PapayaWhip = new Color(255,239,213);
+        public static final Color BlanchedAlmond = new Color(255,235,205);
+        public static final Color Bisque = new Color(255,228,196);
+        public static final Color PeachPuff = new Color(255, 218, 185);
+        public static final Color Moccasin = new Color(255, 228,181);
+        public static final Color Cornsilk = new Color(255, 248, 220);
+        public static final Color Ivory = new Color(255, 255, 240);
+        public static final Color LemonChiffon = new Color(255, 250, 205);
+        public static final Color Seashell = new Color(255, 245, 238);
+        public static final Color Honeydew = new Color(240, 255, 240);
+        public static final Color MintCream = new Color(245, 255, 250);
+        public static final Color Azure = new Color(240, 255, 255);
+        public static final Color AliceBlue = new Color(240, 248, 255);
+        public static final Color lavender = new Color(230, 230, 250);
+        public static final Color LavenderBlush = new Color(255, 240, 245);
+        public static final Color MistyRose = new Color(255, 228, 225);
+}
