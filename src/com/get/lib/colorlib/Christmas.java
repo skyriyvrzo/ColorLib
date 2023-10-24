5c5757539f24aec9cc0f55cfd2fb1ca7c9288eb8
GE77 <skyriyvrzo@gmail.com>
2023-10-24 11:43:32 +0700

    Create Christmas.java

---
+++ src/com/get/lib/colorlib/Christmas.java
@@ -0,0 +1,13 @@
+package com.get.lib.colorlib;
+
+import java.awt.*;
+
+public class Christmas {
+
+    public static final Color Red = new Color(255,0,0);
+    public static final Color LightRed = new Color(255,120,120);
+    public static final Color White = new Color(255,255,255);
+    public static final Color LightGreen = new Color(116,214,128);
+    public static final Color Green = new Color(55,139,41);
+    public static final Color Yellow = new Color(255,217,125);
+}
